4fc2ddc4e6133b3d4c7edbac9c9d2f466731e0c3
German MB <gemboedu@gmail.com>
2021-08-23 21:25:20 -0400

    01 Hola Mundo

---
+++ HolaMundo/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Mi primer mensaje");
+        System.out.println("Hola Mundo desde Java");
+        System.out.println("Iniciando mi curso de java");
+    }
+}
